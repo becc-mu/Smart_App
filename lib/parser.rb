395d3576288e85c70f4ca070705f8124863b3320
becc-mu <becc.mulugeta@gmail.com>
2019-06-21 08:58:11 +0100

    smallest possible test

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,9 @@
 class Parser
+  def Initialize(filename, process_data)
+    @filename = filename
+    @process_data = Processor.new(process_data)
+  end
+  def parse_file
+  end
+
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,14 +2,28 @@ require 'parser'
 
 describe Parser do
   describe '#Initialise' do
-  let(:described_class) { Parser }
-  let(:filename) { 'filename' }
+    let(:described_class) { Parser }
+    let(:filename) { 'filename' }
 
     it '.creates parser class' do
       expect(described_class).to eq Parser
     end
     it 'assignes filename' do
       expect(filename).to eq filename
+    end
+  end
+
+  describe '#processor' do
+    let(:processor) { double :processor, new: records}
+    let(:filename) { 'filename'}
+    context '#raise error' do
+      before do
+        allow(File).to receive(:open) { raise error }
+      end
+      it 'raise error' do
+        expect {  subject.parse_file }.to raise_error('File not found!') unless File.exist?(filename)
+        subject.parse_file(filename, processor)
+      end
     end
   end
 end
