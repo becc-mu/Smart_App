96decc570a61b26143754b793c218c5069c66852
becc-mu <becc.mulugeta@gmail.com>
2019-06-21 08:54:36 +0100

    modify dependency files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'simplecov'
 require 'simplecov-console'
 
@@ -7,13 +5,15 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
   SimpleCov::Formatter::Console
 ]
 SimpleCov.start
-
 RSpec.configure do |config|
+
   config.expect_with :rspec do |expectations|
+
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
+
     mocks.verify_partial_doubles = true
   end
 
