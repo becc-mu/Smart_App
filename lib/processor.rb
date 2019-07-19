a9ac9a772fdf3e0cf1d10c16bfd86a2d921cb5a8
becc-mu <becc.mulugeta@gmail.com>
2019-07-19 11:21:38 +0100

    testing processor methods

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Processor
-  attr_reader :records
-  def Initialize
-    @records = {}
+  attr_reader :results
+  def initialize(results)
+    @results = results
   end
 
-  def process_data
-    
+  def process_data(results)
+    total_views_count = results.each_with_object({})
   end
 end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -1,15 +1,32 @@
+# frozen_string_literal: true
+
+# frozen_string_literal: true.
+
 require 'processor'
 describe Processor do
-  describe '#Initialize' do
-    let(:records) { {} }
-    subject(:processor) { described_class.new }
+    subject(:processor) { described_class.new(results) }
+    let(:results) { log_data }
 
-    it 'adds records' do
-      expect(records).to eq( {})
+    it 'updates log data enteries' do
+      total_views_count = processor.total_views_count
+      expect(page_view_count['/help_page/1']).to eq(4)
     end
-    it 'updates enteries' do
-      expect(subject.process_data).to respond_to(:hash, :class, :to_s)
-    end
+
+  def log_data
+    [
+      { page: '/index', ip: '126.318.035.038' },
+      { page: '/contact', ip: '184.123.665.067' },
+      { page: '/home', ip: '184.123.665.067' },
+      { page: '/about/2', ip: '444.701.448.104' },
+      { page: '/help_page/1', ip: '929.398.951.889' },
+      { page: '/index', ip: '444.701.448.104' },
+      { page: '/home', ip: '184.123.665.067' },
+      { page: '/help_page/1', ip: '929.398.951.889' },
+      { page: '/about', ip: '061.945.150.735' },
+      { page: '/help_page/1', ip: '929.398.951.889' },
+      { page: '/home', ip: '235.313.352.950' },
+      { page: '/contact', ip: '184.123.665.067' },
+      { page: '/help_page/1', ip: '543.910.244.929' }
+    ]
   end
-
 end
